ad53d3a844ebd5d674e3ca9ad78fdb956dc1c548
Jose Guadalupe Vidal Anaya <josevidalanaya@gmail.com>
2020-05-22 19:43:44 -0500

    add normalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 import { Title } from './components'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <Title level='h3'>Edit <code>src/App.tsx</code> and save to reload.</Title>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-            </header>
-        </div>
-    );
+    return <Title>Edit <code>src/App.tsx</code> and save to reload.</Title>
+    
 }
 
 export default App
